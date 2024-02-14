886b99d2d675897c62ae162eaf31d82aca8c3ca4
aho4ahoaho <aho4ahoaho@outlook.jp>
2024-02-15 03:47:50 +0900

    eslintrcのフォーマット

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json"
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
     },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
+    plugins: ["@typescript-eslint", "react"],
+    rules: {
         "@typescript-eslint/no-floating-promises": "off",
         "prefer-const": "off",
         "no-useless-escape": "off",
         "react/react-in-jsx-scope": "off",
     },
-    "ignorePatterns": ["node_modules", "dist", "vite.config.ts"]
-}
+    ignorePatterns: ["node_modules", "dist", "vite.config.ts", ".eslintrc.cjs"],
+};
